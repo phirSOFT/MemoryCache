4f9a0bb97c8325c341087dee76d8890a785df71c
Philemon Eichin <phileicht@gmx.de>
2019-02-24 22:24:44 +0100

    Add UtilityTest

---
+++ MemoryCache.Test/UtilityTest.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+namespace MemoryCache.Test
+{
+    [TestFixture]
+    public class UtilityTest
+    {
+        private const int testNumbers = 10;
+
+        [Test]
+        public void TestIsPowerOfTwo_int_Success([Random(0, 30, 10)] int power)
+        {
+            int testNumber = (1 << power);
+            Assert.True(testNumber.IsPowerOfTwo(), "Positive value");
+            Assert.True((-testNumber).IsPowerOfTwo(),"negative value");
+        }
+
+        [Test]
+        public void TestIsPowerOfTwo_uint_Success([Random(0, 31, 10)] int power)
+        {
+            uint testNumber = (uint)(1 << power);
+            Assert.True(testNumber.IsPowerOfTwo(),"Positive value");
+        }
+
+        [Test]
+        public void TestIsPowerOfTwo_long_Success([Random(0, 62, 10)] int power)
+        {
+            long testNumber = (1L << power);
+            Assert.True(testNumber.IsPowerOfTwo(),"Positive value");
+            Assert.True((-testNumber).IsPowerOfTwo(),"negative value");
+        }
+
+        [Test]
+        public void TestIsPowerOfTwo_ulong_Success([Random(0, 63, 10)] int power)
+        {
+            ulong testNumber = (ulong)(1L << power);
+            Assert.True(testNumber.IsPowerOfTwo(),"Positive value");
+        }
+
+        [Test]
+        public void TestIsPowerOfTwo_ulong_Fail([ValueSource(nameof(ULongFailValues))]ulong number)
+        {
+            Assert.False(number.IsPowerOfTwo());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwo_long_Fail([ValueSource(nameof(LongFailValues))]long number)
+        {
+            Assert.False(number.IsPowerOfTwo());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwo_uint_Fail([ValueSource(nameof(UIntFailValues))]uint number)
+        {
+            Assert.False(number.IsPowerOfTwo());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwo_int_Fail([ValueSource(nameof(IntFailValues))]int number)
+        {
+            Assert.False(number.IsPowerOfTwo());
+        }
+
+
+
+        public static IEnumerable<ulong> ULongFailValues()
+        {
+            return NotPowersOfTwo(testNumbers, 63, false);
+        }
+
+        public static IEnumerable<long> LongFailValues()
+        {
+            return NotPowersOfTwo(testNumbers, 62, true).Select(l => (long) l);
+        }
+
+        public static IEnumerable<uint> UIntFailValues()
+        {
+            return NotPowersOfTwo(testNumbers, 30, false).Select(l => unchecked ((uint) l));
+        }
+
+        public static IEnumerable<int> IntFailValues()
+        {
+            return NotPowersOfTwo(testNumbers, 31, true).Select(l => unchecked ((int) l));
+        }
+        public static IEnumerable<ulong> NotPowersOfTwo(int amountNumbers, int maxDigits, bool sign)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < testNumbers; i++)
+            {
+                var digits = rnd.Next(2, maxDigits);
+                var invert = false;
+                if (digits > maxDigits / 2)
+                {
+                    digits -= maxDigits / 2;
+                    invert = true;
+                }
+
+                var places = Enumerable.Range(1, maxDigits).ToList();
+                ulong value = 0;
+                for (int j = 0; j < digits; j++)
+                {
+                    var digit = places[rnd.Next((places.Count()))];
+                    value |= (1UL << digit);
+                    places.Remove((digit));
+                }
+
+                if (invert)
+                {
+                    value = ~value;
+                }
+
+                yield return value;
+                if (sign)
+                    yield return ~value + (ulong)(1L << (maxDigits > 31 ? 63 : 31));
+
+            }
+        }
+    }
+}
+
--- MemoryCache/Utility.cs
+++ MemoryCache/Utility.cs
@@ -1,25 +1,28 @@
-﻿namespace MemoryCache
+﻿using System;
+
+namespace MemoryCache
 {
     internal static class Utility
     {
         public static bool IsPowerOfTwo(this int number)
         {
-            return (number >= 0 ? (number & (number - 1)) :  (~number & (~number - 1))) == 0;
+            return number < 0 ? IsPowerOfTwo((uint)-number) : IsPowerOfTwo((uint) number);
         }
 
         public static bool IsPowerOfTwo(this uint number)
         {
-            return (number & (number - 1)) == 0;
+            return number <=2  ||(number & (number - 1)) == 0;
         }
 
         public static bool IsPowerOfTwo(this long number)
         {
-            return (number >= 0 ? (number & (number - 1)) :  (~number & (~number - 1))) == 0;
+            return number < 0 ? IsPowerOfTwo((ulong)-number) : IsPowerOfTwo((ulong) number);
         }
 
         public static bool IsPowerOfTwo(this ulong number)
         {
-            return (number & (number - 1)) == 0;
+            
+            return number <=2  || (number & (number - 1)) == 0;
         }
     }
 }
