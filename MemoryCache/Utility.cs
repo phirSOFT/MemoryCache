236782593819664eea42d45f1d4453f72bd36616
Philemon Eichin <phileicht@gmx.de>
2019-02-24 20:54:09 +0100

    AddUtility

---
+++ MemoryCache/Utility.cs
@@ -0,0 +1,25 @@
+﻿namespace MemoryCache
+{
+    internal static class Utility
+    {
+        public static bool IsPowerOfTwo(this int number)
+        {
+            return (number >= 0 ? (number & (number - 1)) :  (~number & (~number - 1))) == 0;
+        }
+
+        public static bool IsPowerOfTwo(this uint number)
+        {
+            return (number & (number - 1)) == 0;
+        }
+
+        public static bool IsPowerOfTwo(this long number)
+        {
+            return (number >= 0 ? (number & (number - 1)) :  (~number & (~number - 1))) == 0;
+        }
+
+        public static bool IsPowerOfTwo(this ulong number)
+        {
+            return (number & (number - 1)) == 0;
+        }
+    }
+}
